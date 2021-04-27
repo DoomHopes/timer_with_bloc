1a3362ed978c8bd958b7713b77de3637bbc52f8e
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-27 15:57:57 +0300

    add Ticker to TimerBloc

--- lib/bloc/timer_bloc.dart
+++ lib/bloc/timer_bloc.dart
@@ -1,12 +1,22 @@
-import 'package:bloc/bloc.dart';
+import 'dart:async';
 
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+import 'ticker.dart';
 import 'timer_event.dart';
 import 'timer_state.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
   static const int _duration = 60;
 
-  TimerBloc() : super(TimerInitial(_duration));
+  final Ticker _ticker;
+  StreamSubscription<int> _tickerSubscription;
+
+  TimerBloc({@required Ticker ticker})
+      : assert(ticker != null),
+        _ticker = ticker,
+        super(TimerInitial(_duration));
 
   @override
   Stream<TimerState> mapEventToState(
